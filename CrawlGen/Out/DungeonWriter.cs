da1a47caf3f44ddd2005278b1a1e33988d87b870
Steven Wokke <git.desktop@stevenw.nl>
2022-08-14 16:14:25 +0200

    Shit, why was this ignored. It isn't as if .gitignore mentions an out folder

---
+++ CrawlGen/Out/DungeonWriter.cs
@@ -0,0 +1,28 @@
+﻿using CrawlGen.Model.Dungeon;
+using CrawlGen.Model.Overworld;
+using CrawlGen.Out.Utils;
+
+namespace CrawlGen.Out
+{
+    public static class DungeonWriter
+    {
+
+        public static void WriteRoom(Room room, HTMLPage page)
+        {
+            page.WriteElem("h2", room.ToString());
+
+            if (room.Treasure.Any())
+                page.WriteElem("p", $"Treasure: {string.Join(", ", room.Treasure)}");
+            // TODO: Monsters
+            // TODO: Doors
+            // TODO: Proper treasure
+        }
+
+        public static void WriteDungeon(Dungeon dungeon, HTMLPage page)
+        {
+            page.WriteElem("h1", dungeon.Name);
+            foreach (var room in dungeon.Map.Rooms)
+                WriteRoom(room, page);
+        }
+    }
+}
