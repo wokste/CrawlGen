4427744d277756f7842bea0c8927ee7e683bb7ac
Steven Wokke <git.desktop@stevenw.nl>
2022-08-16 22:20:13 +0200

    Goddamnit, again missing files. What is it with VS2019 and an Out directory?

---
+++ CrawlGen/Writers/SettlementWriter.cs
@@ -0,0 +1,24 @@
+﻿using CrawlGen.Model.Overworld;
+using CrawlGen.Out.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlGen.Out
+{
+    internal static class SettlementWriter
+    {
+        internal static void WriteTown(Settlement town, HTMLPage page)
+        {
+            page.WriteElem("h1", town.Name);
+
+            page.WriteElem("p", $"Type: {town.Type}");
+            // TODO: Rumour table
+            // TODO: Specific buildings
+            // TODO: Henchmen
+            // TODO: Rulers, factions, etc
+        }
+    }
+}
